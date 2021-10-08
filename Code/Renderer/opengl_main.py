0feede654370617d6a71f9ba4ba20e7d3d53b9fc
madex <madexio@gmail.com>
2021-10-07 17:58:16 -0700

    Importing and testing opengl functionality from a tutorial

--- Code/Renderer/opengl_main.py
+++ Code/Renderer/opengl_main.py
@@ -1,6 +1,47 @@
 import OpenGL
-import OpenGL.GL
-import OpenGL.GLUT
-import OpenGL.GLU
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
 
-print("Hello World")
+w, h = 500, 500
+
+pos = 10
+
+
+def square():
+    global pos
+    glBegin(GL_QUADS)
+    glVertex2f(100 + pos, 100)
+    glVertex2f(200 + pos, 100)
+    glVertex2f(200 + pos, 200)
+    glVertex2f(100 + pos, 200)
+    glEnd()
+    pos += 1
+
+
+def iterate():
+    glViewport(0, 0, 500, 500)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(0.0, 500, 0.0, 500, 0.0, 1.0)
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
+def showScreen():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+    iterate()
+    glColor3f(1.0, 0.0, 3.0)
+    square()
+    glutSwapBuffers()
+
+
+glutInit()
+glutInitDisplayMode(GLUT_RGBA)
+glutInitWindowSize(500, 500)
+glutInitWindowPosition(0, 0)
+wind = glutCreateWindow("OpenGL Coding Practice")
+glutDisplayFunc(showScreen)
+glutIdleFunc(showScreen)
+glutMainLoop()
